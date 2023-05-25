17ce1f521d19288fdaa414478478a58db5546ac0
ChoiWonYu <phillip07290@naver.com>
2023-05-25 23:13:45 +0900

    feat: stackqueue/sock

---
+++ src/Programmers/stackqueue/Stock.java
@@ -0,0 +1,27 @@
+package Programmers.stackqueue;
+
+public class Stock {
+    public static int[] solution(int[] prices) {
+        int[] ans = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                ans[i]++;
+                if (prices[i] > prices[j])
+                    break;
+
+            }
+        }
+
+        return ans;
+    }
+
+
+    public static void main(String[] args){
+        int []arr={1, 2, 3, 2, 3};
+        int []answer=solution(arr);
+        for(int i:answer){
+//            System.out.println(i);
+        }
+    }
+}
---
+++ src/Programmers/stackqueue/Truck.java
@@ -0,0 +1,8 @@
+package Programmers.stackqueue;
+import java.util.*;
+
+public class Truck {
+    public static void main(String[] args){
+        Queue<Integer> q=new LinkedList<Integer>();
+    }
+}
