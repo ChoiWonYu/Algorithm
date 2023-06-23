ec7aff9dc1e0faa7a1a8b55809b43e591d527e0d
ChoiWonYu <phillip07290@naver.com>
2023-06-23 14:20:48 +0900

    refactor:Basic/ClosestWord

--- src/Programmers/Basic/ClosestWord.java
+++ src/Programmers/Basic/ClosestWord.java
@@ -7,19 +7,15 @@ public class ClosestWord {
         int[] answer=new int[s.length()];
 
         for(int i=0;i<s.length();i++){
-            if(m.get(s.charAt(i))==null){
-                answer[i]=-1;
-                m.put(s.charAt(i),i);
-                continue;
-            }
-            answer[i]=i-(int)(m.get(s.charAt(i)));
+            char targetChar=s.charAt(i);
+            answer[i]=i-(int)m.getOrDefault(targetChar,i+1);
             m.put(s.charAt(i),i);
 
         }
         return answer;
     }
     public static void main(String[] main){
-        int[] answer=solution("foobar");
+        int[] answer=solution("banana");
         for(int i:answer){
             System.out.println(i);
 
