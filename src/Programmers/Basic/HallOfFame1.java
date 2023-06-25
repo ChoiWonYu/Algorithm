82f0d6cd4a86e892e75512aa9fbfb1f73e0007fd
ChoiWonYu <phillip07290@naver.com>
2023-06-25 09:23:19 +0900

    refactor: Basic/HallOfFame1

--- src/Programmers/Basic/HallOfFame1.java
+++ src/Programmers/Basic/HallOfFame1.java
@@ -3,20 +3,21 @@ import java.util.*;
 
 public class HallOfFame1 {
         public static int[] solution(int k, int[] score) {
-            List l=new ArrayList<>();
+            PriorityQueue pq=new PriorityQueue<Integer>();
             int[] answer = new int[score.length];
             int idx=0;
             for(int i:score){
-                l.add(i);
-                l.sort(Collections.reverseOrder());
-                answer[idx++]=l.size()<k?(int)l.get(l.size()-1):(int)l.get(k-1);
+                pq.add(i);
+                if(pq.size()>k)pq.poll();
+
+                answer[idx++]=(int)pq.peek();
             }
 
             return answer;
         }
     public static void main(String[] args){
-        int[ ] socore={0, 300, 40, 300, 20, 70, 150, 50, 500, 1000};
-        int []result=solution(4,socore);
+        int[ ] socore={10, 100, 20, 150, 1, 100, 200};
+        int []result=solution(3,socore);
         System.out.println(Arrays.toString(result));
     }
 }
