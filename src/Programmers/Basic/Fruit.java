1bafa9d17894da9052add8056e9023d7bfba6c71
ChoiWonYu <phillip07290@naver.com>
2023-06-25 10:16:26 +0900

    refactor: Basic/Fruit

--- src/Programmers/Basic/Fruit.java
+++ src/Programmers/Basic/Fruit.java
@@ -3,26 +3,16 @@ import java.util.*;
 
 public class Fruit {
     public static int solution(int k, int m, int[] score) {
-        ArrayList l=new ArrayList();
+        Arrays.sort(score);
         int answer = 0;
-
-
-        for(int i:score){
-            l.add(i);
-        }
-        Collections.sort(l);
-
-        while(l.size()%m!=0){
-            l.remove(0);
-        }
-        for(int i=0;i<l.size();i+=m){
-            answer+=(int)l.get(i)*m;
+        for(int i=score.length;i>=m;i-=m){
+            answer+=score[i-m]*m;
         }
         return answer;
     }
 
     public static void main(String[] args){
-        int result=solution(4,15, new int[] {4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2});
+        int result=solution(4,3, new int[] {4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2});
         System.out.println(result);
     }
 
