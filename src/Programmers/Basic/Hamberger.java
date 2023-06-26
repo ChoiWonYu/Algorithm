3c4ae32b947036f83976f9419f6a4dd197fbe510
ChoiWonYu <phillip07290@naver.com>
2023-06-26 14:57:34 +0900

    feat:Basic/Hamberger comment

--- src/Programmers/Basic/Hamberger.java
+++ src/Programmers/Basic/Hamberger.java
@@ -12,9 +12,10 @@ public class Hamberger {
             stack[sp++]=i;
             if(sp>=4&&stack[sp-1]==1&&stack[sp-2]==3&&stack[sp-3]==2&&stack[sp-4]==1){
                 sp-=4;
+                //스택 포인터를 감소시켜 그 후 원소를 배열에 덮어쓰면 필요없는 원소를 삭제하고 그 뒤 원소를 당길 필요 없어짐
                 answer++;
             }
-        }g
+        }
         return answer;
     }
 
