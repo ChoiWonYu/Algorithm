302c940a79ab0fd9037c182b678435548e6f23ab
ChoiWonYu <phillip07290@naver.com>
2023-06-27 13:51:21 +0900

    feat: NumberTheory/Combination

--- src/AlgorithmNote/NumberTheory/Combination.java
+++ src/AlgorithmNote/NumberTheory/Combination.java
@@ -1,5 +1,17 @@
 package AlgorithmNote.NumberTheory;
 
 public class Combination {
+    public static void combination(int[] arr,boolean[] visited, int start,int n,int r){
+        if(r==0){
+            //visited에 선택됨
+        }
+        for(int i=start;i<n;i++){
+            visited[i]=true;
+            combination(arr,visited,start+1,n,r-1);
+            visited[i]=false;
+        }
+    }
+    public static void main(String[] args){
 
+    }
 }
---
+++ src/Programmers/Basic/Trio.java
@@ -0,0 +1,14 @@
+package Programmers.Basic;
+
+public class Trio {
+    public static int solution(int[] number){
+        return 1;
+    }
+
+    public static void main(String[] args){
+        int[] nums=new int[]{-2, 3, 0, 2, -5};
+        int result=solution(nums);
+        System.out.println(result);
+    }
+
+}
