c16bf02ce10645ebf0134c6fbd1629c7f996f9ec
ChoiWonYu <phillip07290@naver.com>
2023-07-01 00:19:25 +0900

    refactor: Basic/Measure

--- src/Programmers/Basic/Measure.java
+++ src/Programmers/Basic/Measure.java
@@ -1,26 +1,13 @@
 package Programmers.Basic;
-import java.util.*;
 
 public class Measure {
-
-    public static Set getMeasure(int num){
-        Set<Integer> s=new HashSet<>();
-        for(int i=1;i<=Math.sqrt(num);i++){
-            if(num%i==0) {
-                s.add(i);
-                s.add(num/i);
-            }
-            }
-        return s;
-        }
-
-
-
     public static int solution(int left, int right) {
         int answer = 0;
         for(int i=left;i<=right;i++){
-            int size=getMeasure(i).size();
-            answer+=size%2==0?i:-i;
+            if(i%Math.sqrt(i)==0){
+                answer-=i;
+            }
+            else answer+=i;
         }
         return answer;
     }
