8fdb298c42814f3194bcaf860c679c81264f77ea
ChoiWonYu <phillip07290@naver.com>
2023-07-11 15:12:45 +0900

    refactor: Basic/HarshadNumber

--- src/Programmers/Basic/HarshadNumber.java
+++ src/Programmers/Basic/HarshadNumber.java
@@ -1,13 +1,15 @@
 package Programmers.Basic;
 
+import java.util.Arrays;
+
 public class HarshadNumber {
     public static boolean solution(int x) {
-        String num=Integer.toString(x);
-        int sum=0;
-        for(int i=0;i<num.length();i++){
-            sum+=Integer.parseInt(num.substring(i,i+1));
-        }
-        return x%sum==0?true:false;
+        int sum= Arrays.stream(Integer.toString(x).split(""))
+                .mapToInt(st->Integer.valueOf(st))
+                .sum();
+//        int sum=num.chars().map(ch->ch-'0').sum();
+
+        return x%sum==0;
     }
 
     public static void main(String[] args) {
