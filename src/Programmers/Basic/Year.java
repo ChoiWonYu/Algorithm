47bd3744e7e6165de768acbf7c0cbd3e08bba29f
ChoiWonYu <phillip07290@naver.com>
2023-07-14 15:35:25 +0900

    refactor: Basic/Year

--- src/Programmers/Basic/Year.java
+++ src/Programmers/Basic/Year.java
@@ -3,24 +3,12 @@ package Programmers.Basic;
 import java.util.*;
 
 public class Year {
-    public int getTotalDateUntilPrev(int month){
-        if(month==1)return 0;
-        Map<Integer,Integer>m=new HashMap();
-        for(int i=1;i<=12;i++)m.put(i,31);
-        m.put(2,29);
-        m.put(4,30);
-        m.put(6,30);
-        m.put(9,30);
-        m.put(11,30);
-
-        return m.keySet().stream().filter(k->k<month)
-                .mapToInt(k->m.get(k)).sum();
-    }
-
     public String solution(int a, int b) {
-        String[] Day={"FRI","SAT","SUN","MON","TUE","WED","THU"};
-
-        return Day[(getTotalDateUntilPrev(a)+b-1)%7];
+        String[] day={"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int[] days={31,29,31,30,31,30,31,31,30,31,30,31};
+        int total=0;
+        for(int i=0;i<a-1;i++)total+=days[i];
+        return day[(total+b-1)%7];
     }
 
     public static void main(String[] args) {
