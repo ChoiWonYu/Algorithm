9b105c6433c8e89abcbbd649ad1f4ee3ade50c49
ChoiWonYu <phillip07290@naver.com>
2023-07-19 19:27:29 +0900

    feat: inflearn/twopointer/ContinuousSequence

---
+++ src/inflearn/twopointer/ContinuousSequence.java
@@ -0,0 +1,49 @@
+package inflearn.twopointer;
+
+import java.util.*;
+
+public class ContinuousSequence {
+  public static int solution(int n,int[] arr) {
+    if (arr.length == 1)
+      return n == arr[0] ? 1 : 0;
+    int cnt = 0;
+    int sum = 0;
+    int p1 = 0;
+    int p2 = 0;
+    while (p2 < arr.length) {
+      if (sum == n) {
+        cnt++;
+        sum -= arr[p1++];
+        sum += arr[p2++];
+      } else if (sum > n) {
+        sum -= arr[p1++];
+      } else {
+        sum += arr[p2++];
+      }
+    }
+    while(p1<arr.length){
+      if (sum == n) {
+        cnt++;
+        break;
+      }
+      else{
+        sum-=arr[p1++];
+      }
+    }
+    return cnt;
+  }
+
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n=sc.nextInt();
+    int m=sc.nextInt();
+
+    int[] arr = new int[n];
+    for(int i=0;i<n;i++)arr[i]=sc.nextInt();
+
+    int result = solution(m, arr);
+    System.out.println(result);
+  }
+
+}
