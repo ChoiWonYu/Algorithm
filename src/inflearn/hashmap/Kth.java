aea39593b934a5fdb6b84b58cc1c0c652b47f128
ChoiWonYu <phillip07290@naver.com>
2023-07-21 17:44:51 +0900

    refactor: inflearn/hashmap/Kth

--- src/inflearn/hashmap/Kth.java
+++ src/inflearn/hashmap/Kth.java
@@ -3,45 +3,34 @@ package inflearn.hashmap;
 import java.util.*;
 
 public class Kth {
-  static Set<Integer>answer=new HashSet<>();
 
-  public static void combination(int[] arr, boolean[] visited, int r,int n) {
-    if(r==0){
-      int sum=0;
-      for (int i = 0; i < visited.length; i++) {
-        if(visited[i]) {
-          sum += arr[i];
+  public static int solution(int[] arr, int k) {
+    Set<Integer> s = new TreeSet(Collections.reverseOrder());
+    int answer=-1;
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        for (int l = j + 1; l < arr.length; l++) {
+          s.add(arr[i] + arr[j] + arr[l]);
         }
       }
-      answer.add(sum);
-      return;
     }
-
-    for(int i=n;i<arr.length;i++){
-      if(!visited[i]) {
-        visited[i] = true;
-        combination(arr, visited, r - 1, n + 1);
-        visited[i] = false;
-      }
+    int cnt=1;
+    for (int i : s) {
+      if(cnt++==k)return i;
     }
-  }
-
-  public static int[] solution(int[] arr, int k) {
-    boolean[] visited=new boolean[arr.length];
-    combination(arr,visited,k,0);
-    return answer.stream().mapToInt(Integer::intValue).toArray();
+    return answer;
   }
 
   public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    int k=sc.nextInt();
-    int[] arr=new int[n];
-    for(int i=0;i<n;i++)arr[i]=sc.nextInt();
-    int[]result=solution(arr,k);
-
-    Arrays.sort(result);
-    System.out.println(result[result.length-k]);
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    int answer = solution(arr, k);
+    System.out.println(answer);
   }
 
 }
