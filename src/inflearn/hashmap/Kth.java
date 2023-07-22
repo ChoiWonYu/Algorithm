d093a53ce21e993696f3ae3a9cb6f02acd282ced
ChoiWonYu <phillip07290@naver.com>
2023-07-22 09:35:21 +0900

    refactor: inflearn/hashmap/Kth

--- src/inflearn/hashmap/Kth.java
+++ src/inflearn/hashmap/Kth.java
@@ -6,7 +6,7 @@ public class Kth {
 
   public static int solution(int[] arr, int k) {
     Set<Integer> s = new TreeSet(Collections.reverseOrder());
-    int answer=-1;
+    int answer = -1;
     for (int i = 0; i < arr.length; i++) {
       for (int j = i + 1; j < arr.length; j++) {
         for (int l = j + 1; l < arr.length; l++) {
@@ -14,9 +14,11 @@ public class Kth {
         }
       }
     }
-    int cnt=1;
+    int cnt = 1;
     for (int i : s) {
-      if(cnt++==k)return i;
+      if (cnt++ == k) {
+        return i;
+      }
     }
     return answer;
   }
