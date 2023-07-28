28368759b8d2cb68d4479053bba696185c1463e4
ChoiWonYu <phillip07290@naver.com>
2023-07-28 14:27:00 +0900

    feat: DFS basic

--- src/AlgorithmNote/dfsbfs/DFS.java
+++ src/AlgorithmNote/dfsbfs/DFS.java
@@ -1,15 +1,34 @@
 package AlgorithmNote.dfsbfs;
 
+import java.util.*;
+
+class Node<T>{
+  T data;
+  Node lt,rt;
+  public Node(T val){
+    data=val;
+    lt=rt=null;
+  }
+}
+
 public class DFS {
+  static Node<Integer> root;
 
-  public static void dfs(int n) {
-
-    if(n==0)return;
-    System.out.println(n);
-    dfs(n-1);
+  public static void dfs(Node n) {
+    if(n==null)return;
+    dfs(n.lt);
+    System.out.println(n.data);
+    dfs(n.rt);
   }
 
   public static void main(String[] args) {
-    dfs(3);
+    root=new Node<Integer>(1);
+    root.lt=new Node<Integer>(2);
+    root.rt=new Node<Integer>(3);
+    root.lt.lt=new Node<Integer>(4);
+    root.lt.rt=new Node<Integer>(5);
+    root.rt.lt=new Node<Integer>(6);
+    root.rt.rt=new Node<Integer>(7);
+    dfs(root);
   }
 }
