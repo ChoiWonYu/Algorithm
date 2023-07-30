ae9066c266ce9c098365af9fd82bd9e409dac36c
ChoiWonYu <phillip07290@naver.com>
2023-07-30 15:33:41 +0900

    refactor: inflearn/dfsbfs/Calf

--- src/inflearn/dfsbfs/Calf.java
+++ src/inflearn/dfsbfs/Calf.java
@@ -4,34 +4,39 @@ import java.util.*;
 
 public class Calf {
 
+  static int[] dis = {1, -1, 5};
+  static boolean[] visited;
+
   public static int solution(int start, int end) {
+    visited = new boolean[10001];
     Queue<Integer> Q = new LinkedList<>();
+    visited[start] = true;
     Q.offer(start);
-    int minLen=Integer.MAX_VALUE;
-    int level=0;
-    int minDis=0;
-    while (minLen>0) {
-      int len=Q.size();
-      System.out.println(Q.toString());
-      for(int i=0;i<len;i++){
-        int cur=Q.poll();
-        if(Math.abs(end-cur)<minLen) {
-          minDis = cur;
-          minLen = Math.abs(end - cur);
+    int level = 0;
+    while (!Q.isEmpty()) {
+      int len = Q.size();
+      for (int i = 0; i < len; i++) {
+        int cur = Q.poll();
+        for (int j = 0; j < dis.length; j++) {
+          int d = cur + dis[j];
+          if (d == end) {
+            return level + 1;
+          }
+          if (d >= 1 && d <= 10000 && !visited[d]) {
+            visited[d] = true;
+            Q.offer(d);
+          }
         }
       }
-      Q.offer(minDis+1);
-      Q.offer(minDis-1);
-      Q.offer(minDis+5);
       level++;
     }
-    return level-1;
+    return -1;
   }
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int s=sc.nextInt();
-    int e=sc.nextInt();
+    int s = sc.nextInt();
+    int e = sc.nextInt();
 
     int answer = solution(s, e);
     System.out.println(answer);
