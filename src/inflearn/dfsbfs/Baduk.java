dbf21272ea65aa1b13ccace1bdb74eb4888ee8da
ChoiWonYu <phillip07290@naver.com>
2023-08-02 12:06:55 +0900

    feat: inflearn/dfsbfs/Baduk

---
+++ src/inflearn/dfsbfs/Baduk.java
@@ -0,0 +1,31 @@
+package inflearn.dfsbfs;
+
+import java.util.*;
+
+public class Baduk {
+  static int[] weight;
+  static int maxSum=0,maxWeight;
+
+  public static void dfs(int n, int s) {
+    if(s<=maxWeight&&s>maxSum)maxSum=s;
+    if(n==weight.length)return;
+    dfs(n+1,s+weight[n]);
+    dfs(n+1,s);
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int c=sc.nextInt();
+    maxWeight=c;
+
+    int n=sc.nextInt();
+    weight=new int[n+1];
+    for(int i=1;i<=n;i++){
+      weight[i]=sc.nextInt();
+    }
+
+    dfs(0, 0);
+    System.out.println(maxSum);
+  }
+
+}
