249f9bf1a84bbe3671793c348b689069190015bb
ChoiWonYu <phillip07290@naver.com>
2023-08-06 11:10:38 +0900

    refactor: inflearn/bfsdfs/MazeShortestRoute using class

--- src/inflearn/dfsbfs/MazeShortestRoute.java
+++ src/inflearn/dfsbfs/MazeShortestRoute.java
@@ -2,30 +2,55 @@ package inflearn.dfsbfs;
 
 import java.util.*;
 
+class Location {
+
+  private int row;
+  private int col;
+
+  public Location(int row, int col) {
+    this.row = row;
+    this.col = col;
+  }
+
+  public int getRow(){
+    return row;
+  }
+
+  public int getCol(){
+    return col;
+  }
+
+  public boolean isFinish(int max) {
+    return row == max - 1 && col == max - 1;
+  }
+}
+
 public class MazeShortestRoute {
 
   static final int MAX = 7;
-  static int[][] maze = new int[MAX][MAX], route = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-  static boolean[][] visited = new boolean[MAX][MAX];
+  static int[][] maze = new int[MAX][MAX];
+  static int[] rowMove = new int[]{0, 0, 1, -1}, colMove = new int[]{1, -1, 0, 0};
 
   private static int bfs(int row, int col) {
-    Queue<int[]> Q = new LinkedList<>();
-    int count = 0;
-    Q.offer(new int[]{row, col});
+    Queue<Location> Q = new LinkedList<>();
+    Q.offer(new Location(row, col));
+    int count=0;
+
     while (!Q.isEmpty()) {
       int len = Q.size();
       for (int i = 0; i < len; i++) {
-        int[] cur = Q.poll();
-        if (isFinish(cur[0], cur[1])) {
+        Location cur = Q.poll();
+        if (cur.isFinish(MAX)) {
           return count;
         }
-        for (int[] next : route) {
-          int r = cur[0] + next[0], c = cur[1] + next[1];
-          if (!canGo(r, c)) {
+        for (int j=0;j<4;j++) {
+          int x=cur.getRow()+rowMove[j];
+          int y=cur.getCol()+colMove[j];
+          if (!canGo(x,y)) {
             continue;
           }
-          Q.offer(new int[]{r, c});
-          visited[r][c]=true;
+          Q.offer(new Location(x,y));
+          maze[x][y] = 1;
         }
       }
       count++;
@@ -40,18 +65,12 @@ public class MazeShortestRoute {
         maze[i][j] = sc.nextInt();
       }
     }
-    visited[0][0] = true;
     int answer = bfs(0, 0);
     System.out.println(answer);
-
   }
 
   private static boolean canGo(int row, int col) {
-    return row < MAX && col < MAX && row >= 0 && col >= 0 && maze[row][col] != 1
-        && !visited[row][col];
+    return row < MAX && col < MAX && row >= 0 && col >= 0 && maze[row][col] != 1;
   }
 
-  private static boolean isFinish(int row, int col) {
-    return row == MAX - 1 && col == MAX - 1;
-  }
 }
