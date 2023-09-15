9f9f4a31c3e290873fbfeccfdc20d12105a90dbd
WonyuChoi <phillip07290@naver.com>
2023-09-15 14:50:03 +0900

    feat: BOJ/backtracking/NM4

---
+++ src/BOJ/backtracking/NM4.java
@@ -0,0 +1,39 @@
+package BOJ.backtracking;
+
+import java.util.*;
+
+public class NM4 {
+
+    static int N,M;
+    static int[] result,nums;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N=sc.nextInt();
+        M=sc.nextInt();
+        nums = new int[N];
+
+        for(int i=0;i<N;i++)nums[i]=i+1;
+
+        result = new int[M];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == M) {
+            for(int i:result)sb.append(i).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if(depth>0&&i+1<result[depth-1])continue;
+
+            result[depth]=i+1;
+            dfs(depth + 1);
+        }
+    }
+}
