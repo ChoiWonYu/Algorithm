9359d906f954d9a5b50fcc6c352bc7e68b6e7761
WonyuChoi <phillip07290@naver.com>
2023-09-22 15:21:48 +0900

    feat: 기본 템플릿 생성

---
+++ src/template/Basic.java
@@ -0,0 +1,14 @@
+package template;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Basic {
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int i;
+    }
+}
