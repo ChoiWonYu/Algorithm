cc9f5c22cdc3460fc7590a9bbe4d3fc158e3208e
WonyuChoi <phillip07290@naver.com>
2023-09-22 15:30:45 +0900

    feat: DFS 템플릿

---
+++ src/template/Dfs.java
@@ -0,0 +1,22 @@
+package template;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Dfs {
+
+    public static void dfs() {
+        if(true){
+            return;
+        }
+        dfs();
+    }
+
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int i;
+    }
+}
