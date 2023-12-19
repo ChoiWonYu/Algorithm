348186420c2a748e2ccb04a3f3717f4f92bf0476
WonyuChoi <phillip07290@naver.com>
2023-12-19 19:38:34 +0900

    refactor: BOJ/stack/reverse-word string builder reverse

--- src/BOJ/stack/ReverseWord.java
+++ src/BOJ/stack/ReverseWord.java
@@ -2,7 +2,6 @@ package BOJ.stack;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class ReverseWord {
@@ -11,22 +10,17 @@ public class ReverseWord {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        StringBuilder reverseSb = new StringBuilder();
         String target;
-        Stack<Character> s = new Stack<>();
 
         int i,num = Integer.parseInt(br.readLine());
         for (i = 0; i < num; i++) {
             st = new StringTokenizer(br.readLine());
             while(st.hasMoreTokens()){
                 target = st.nextToken();
-                target.chars()
-                    .forEach(ch->{
-                        s.push((char)ch);
-                    });
-                while (!s.empty()) {
-                    sb.append(s.pop());
-                }
-                sb.append(" ");
+                reverseSb.append(target).reverse();
+                sb.append(reverseSb).append(" ");
+                reverseSb.delete(0,reverseSb.length());
             }
             sb.append("\n");
         }
