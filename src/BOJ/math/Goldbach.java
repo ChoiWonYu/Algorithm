2c78269a0b4b620a63f7b7b8cc867aab73b23dd7
WonyuChoi <phillip07290@naver.com>
2023-12-22 17:09:16 +0900

    refactor: BOJ/deque/Goldbach 에라토스테네스의 체 최적화

--- src/BOJ/math/Goldbach.java
+++ src/BOJ/math/Goldbach.java
@@ -6,23 +6,31 @@ import java.io.InputStreamReader;
 
 public class Goldbach {
 
+    private static final int MAX = 1000000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int i, input, mul;
+        int i, input;
 
-        boolean[] noPrime = new boolean[1000001];
-        noPrime[1] = true;
-        for (i = 2; i <= 1000000; i++) {
-            mul = 2;
-            while (i * mul <= 1000000) {
-                noPrime[i * mul++] = true;
+        //에라토스테네스의 체
+        boolean[] isPrime = new boolean[MAX + 1];
+        for (i = 0; i <= MAX; i++) {
+            isPrime[i] = true;
+        }
+        isPrime[0] = isPrime[1] = false;
+
+        for (i = 2; i <= Math.sqrt(MAX); i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= MAX; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
 
         while ((input = Integer.parseInt(br.readLine())) != 0) {
             for (i = 3; i < input; i++) {
-                if (!noPrime[i] && i % 2 == 1 && !noPrime[input - i] && (input - i) % 2 == 1) {
+                if (isPrime[i] && i % 2 == 1 && isPrime[input - i] && (input - i) % 2 == 1) {
                     sb.append(input).append(" = ").append(i).append(" + ").append(input - i)
                         .append("\n");
                     break;
