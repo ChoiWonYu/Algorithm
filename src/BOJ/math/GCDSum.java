fb1905756f56d95130995ee663f8d40e3ee7986c
WonyuChoi <phillip07290@naver.com>
2023-12-22 17:28:40 +0900

    feat: BOJ/math/GCDSum

---
+++ src/BOJ/math/GCDSum.java
@@ -0,0 +1,52 @@
+package BOJ.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class GCDSum {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int[] nums = new int[m];
+            for (int j = 0; j < m; j++) {
+                nums[j] = Integer.parseInt(st.nextToken());
+            }
+            sb.append(solve(nums)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static long solve(final int[] nums) {
+        long result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                result += calculateGCD(nums[i], nums[j]);
+            }
+        }
+        return result;
+    }
+
+    private static int calculateGCD(int num1, int num2) {
+        while (true) {
+            int num3 = num1 % num2;
+            num1 = num2;
+            num2 = num3;
+            if (num3 == 0) {
+                break;
+            }
+        }
+        return num1;
+    }
+
+}
