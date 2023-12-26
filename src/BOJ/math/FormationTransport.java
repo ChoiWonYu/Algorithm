e07434af4682c12d533f6c79fcf54c4b6203bce4
WonyuChoi <phillip07290@naver.com>
2023-12-26 16:55:27 +0900

    feat: BOJ/math/FormationTransport

---
+++ src/BOJ/math/FormationTransport.java
@@ -0,0 +1,20 @@
+package BOJ.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class FormationTransport {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String input = st.nextToken();
+        int radix = Integer.parseInt(st.nextToken());
+
+        System.out.println(Integer.parseInt(input, radix));
+    }
+
+}
