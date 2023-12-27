cb97b65420a40dabd478aeb2b3780ee3be4a83b0
WonyuChoi <phillip07290@naver.com>
2023-12-27 23:48:31 +0900

    refactor: inflearn/dp/LIS

--- src/inflearn/dp/LIS.java
+++ src/inflearn/dp/LIS.java
@@ -11,27 +11,27 @@ public class LIS {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
         int[] nums = new int[n];
 
-        int max = -1;
+        int max, answer = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
             dp[i] = 1;
-
+            max = 0;
             for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                if (nums[j] < nums[i] && max < dp[j]) {
+                    max = dp[j];
                 }
             }
+            dp[i] = max + 1;
 
-            max = Math.max(max, dp[i]);
+            answer = Math.max(answer, dp[i]);
         }
 
-        System.out.println(max);
+        System.out.println(answer);
     }
 }
