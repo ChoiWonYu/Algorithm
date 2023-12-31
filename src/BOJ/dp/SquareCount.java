f66e11d970f8a5de51eada0a355837ca0e8f4532
WonyuChoi <phillip07290@naver.com>
2023-12-31 15:25:53 +0900

    refactor: BOJ/dp/SquareCount 코드 축약

--- src/BOJ/dp/SquareCount.java
+++ src/BOJ/dp/SquareCount.java
@@ -10,26 +10,13 @@ public class SquareCount {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int limit = 0;
-
-        while (Math.pow(limit + 1, 2) <= n) {
-            limit += 1;
-        }
-
-        int[] squares = new int[limit];
-        for (int i = 0; i < limit; i++) {
-            squares[i] = (int) Math.pow(i + 1, 2);
-        }
 
         int[] dp = new int[n + 1];
-        dp[0] = 0;
-        for (int i = 1; i <= n; i++) {
-            dp[i] += dp[i - 1] + 1;
-        }
 
-        for (int i = 0; i < squares.length; i++) {
-            for (int j = squares[i]; j <= n; j++) {
-                dp[j] = Math.min(dp[j - squares[i]] + 1, dp[j]);
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
 
