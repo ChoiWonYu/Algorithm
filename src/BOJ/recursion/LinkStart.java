971cb2654c4950df0118d8f737a70833ae8ba249
WonyuChoi <phillip07290@naver.com>
2024-01-11 17:34:35 +0900

    refactor: BOJ/bruteforce/LinkStart 부분집합을 visited로만 처리

--- src/BOJ/recursion/LinkStart.java
+++ src/BOJ/recursion/LinkStart.java
@@ -2,13 +2,12 @@ package BOJ.recursion;
 
 import java.util.*;
 import java.io.*;
-import java.util.stream.*;
 
 public class LinkStart {
 
     static int n;
     static int[][] graph;
-    static List<Integer> opposite, cur;
+    static boolean[] visited;
     static int result = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
@@ -16,13 +15,7 @@ public class LinkStart {
 
         n = Integer.parseInt(br.readLine());
         graph = new int[n][n];
-        opposite = new ArrayList<>();
-        cur = new ArrayList<>();
-
-        for (int i = 0; i <= n / 2; i++) {
-            opposite.add(0);
-            cur.add(0);
-        }
+        visited = new boolean[n];
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -38,37 +31,39 @@ public class LinkStart {
 
     private static void recursion(final int depth, final int index) {
 
-        if (depth > n / 2) {
+        if (result == 0) {
             return;
         }
 
-        for (int t = index; t < n; t++) {
-            cur.set(depth, t);
-            if (depth <= n / 2) {
-                opposite = IntStream.range(0, n)
-                    .filter(num -> IntStream.range(0, depth + 1)
-                        .noneMatch(idx -> num == cur.get(idx)))
-                    .boxed()
-                    .collect(Collectors.toList());
+        if (depth == n) {
+            int cur = 0;
+            int opposite = 0;
 
-                int oppositeSum = 0;
-                int sum = 0;
-                for (int i = 0; i < opposite.size(); i++) {
-                    for (int j = 0; j < opposite.size(); j++) {
-                        oppositeSum += graph[opposite.get(i)][opposite.get(j)];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (visited[i] != visited[j]) {
+                        continue;
+                    }
+
+                    if (visited[i]) {
+                        cur += graph[j][i];
+                    } else {
+                        opposite += graph[j][i];
                     }
                 }
-
-                for (int i = 0; i <= depth; i++) {
-                    for (int j = 0; j <= depth; j++) {
-                        sum += graph[cur.get(i)][cur.get(j)];
-                    }
-                }
-                result = Math.min(result, Math.abs(oppositeSum - sum));
             }
-            recursion(depth + 1, t + 1);
+            result = Math.min(Math.abs(opposite - cur), result);
+            return;
         }
-
+        for (int i = index; i < n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            recursion(depth + 1, i + 1);
+            visited[i] = false;
+            recursion(depth + 1, i + 1);
+        }
     }
 
 }
