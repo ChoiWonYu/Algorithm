a2a983b125fcb6c2ee5418ac5d2c431c113f6a0b
WonyuChoi <phillip07290@naver.com>
2024-01-20 12:12:17 +0900

    refactor: BOJ/graph/HideAndSeek4 from 배열로 이전 기록을 트래킹

--- src/BOJ/bfs/HideAndSeek4.java
+++ src/BOJ/bfs/HideAndSeek4.java
@@ -1,90 +1,78 @@
 package BOJ.bfs;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class HideAndSeek4 {
 
-    static boolean[] visited = new boolean[100001];
-    static int maxNum = 100000;
+    static int n;
+    static int[] from;
+    static boolean[] visited;
+    static Deque<Integer> d = new LinkedList<>();
 
-    public static int bfs(int startNum, int dest) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(startNum);
-        int cnt = 0, len, cur, num, minusNum, plusNum;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (!q.isEmpty()) {
-            len = q.size();
-            for (int i = 0; i < len; i++) {
-                cur = q.poll();
-                q.offer(cur);
-                num = cur;
-                if (num == dest) {
-                    return cnt;
-                }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-                while (true) {
-                    if (num == 0) {
-                        break;
-                    }
-                    num *= 2;
-                    if (num == dest) {
-                        return cnt;
-                    }
-                    if (num > maxNum) {
-                        break;
-                    }
-                    if (visited[num]) {
-                        continue;
-                    }
+        visited = new boolean[100001];
+        visited[n] = true;
+        from = new int[100001];
+        from[n] = -1;
+        d.add(n);
 
-                    visited[num] = true;
-                    q.offer(num);
-
-                    if (num > dest) {
-                        break;
-                    }
-                }
-            }
-            len = q.size();
+        int cnt = 0;
+        while (!d.isEmpty()) {
+            int len = d.size();
             cnt++;
             for (int i = 0; i < len; i++) {
-                cur = q.poll();
-                num = cur;
-                minusNum = num - 1;
-                plusNum = num + 1;
-
-                if (minusNum == dest) {
-                    return cnt;
-                }
-                if (plusNum == dest) {
-                    return cnt;
-
+                int cur = d.poll();
+                if (cur == k) {
+                    System.out.println(cnt - 1);
+                    printResult(cur);
+                    return;
                 }
 
-                if (minusNum >= 0 && !visited[minusNum]) {
-                    q.offer(minusNum);
-                    visited[minusNum] = true;
+                int forward = cur + 1;
+                int back = cur - 1;
+                int multi = cur * 2;
+
+                if (cur < k && forward <= 100000 && !visited[forward]) {
+                    addNum(forward, cur);
                 }
 
-                if (plusNum <= maxNum && !visited[plusNum]) {
-                    q.offer(plusNum);
-                    visited[plusNum] = true;
+                if (back >= 0 && !visited[back]) {
+                    addNum(back, cur);
+                }
+
+                if (cur < k && multi <= 100000 && !visited[multi]) {
+                    addNum(multi, cur);
                 }
             }
         }
-        return cnt;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    private static void printResult(final int cur) {
+        Stack<Integer> s = new Stack<>();
+        int next = cur;
+        while (next != -1) {
+            s.push(next);
+            next = from[next];
+        }
 
-        int count = bfs(n, m);
+        StringBuilder sb = new StringBuilder();
+        while (!s.isEmpty()) {
+            sb.append(s.pop()).append(" ");
+        }
 
-        System.out.println(count);
+        System.out.println(sb);
     }
 
+    private static void addNum(final int num, final int cur) {
+        from[num] = cur;
+        visited[num] = true;
+        d.add(num);
+    }
 }
