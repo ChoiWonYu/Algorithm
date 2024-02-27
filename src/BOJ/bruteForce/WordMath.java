d4c7649485b04824debbc5583c50eea00dc95230
WonyuChoi <phillip07290@naver.com>
2024-02-27 13:32:33 +0900

    refactor: boj/bruteForce/WordMath 알파벳을 이용한 배열

--- src/BOJ/bruteForce/WordMath.java
+++ src/BOJ/bruteForce/WordMath.java
@@ -3,73 +3,31 @@ package BOJ.bruteForce;
 import java.io.*;
 import java.util.*;
 
-class Alphabet implements Comparable<Alphabet> {
-
-    char a;
-    int n = 0;
-
-    public Alphabet(char ch) {
-        a = ch;
-    }
-
-    public void add(int n) {
-        this.n += n;
-    }
-
-    public void multiple(int n) {
-        this.n *= n;
-    }
-
-    @Override
-    public int compareTo(Alphabet o) {
-        return o.n - n;
-    }
-}
-
 public class WordMath {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        int[] alphas = new int[26];
         int n = Integer.parseInt(br.readLine());
-        HashSet<Character> s = new HashSet<>();
-        String[] inputs = new String[n];
         for (int i = 0; i < n; i++) {
             String input = br.readLine();
-            inputs[i] = input;
-            for (int j = 0; j < input.length(); j++) {
-                s.add(input.charAt(j));
-            }
-        }
-        int len = s.size();
-        List<Alphabet> alphabets = new ArrayList<>();
-        for (char ch : s) {
-            alphabets.add(new Alphabet(ch));
-        }
-
-        for (String str : inputs) {
             int mul = 1;
-            for (int i = str.length() - 1; i >= 0; i--) {
-                char target = str.charAt(i);
-                for (Alphabet a : alphabets) {
-                    if (a.a == target) {
-                        a.add(mul);
-                        break;
-                    }
-                }
+            for (int j = input.length() - 1; j >= 0; j--) {
+                char target = input.charAt(j);
+                alphas[target - 'A'] += mul;
                 mul *= 10;
             }
         }
-
-        Collections.sort(alphabets);
+        Arrays.sort(alphas);
 
         int start = 9;
         int sum = 0;
-        for (Alphabet al : alphabets) {
-            al.multiple(start--);
-            sum += al.n;
+        for (int i = 25; i >= 0; i--) {
+            if (alphas[i] == 0) {
+                break;
+            }
+            sum += alphas[i] * start--;
         }
-
         System.out.println(sum);
     }
 
