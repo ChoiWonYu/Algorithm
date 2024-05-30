1a6896361c263ab82f9a4c25df5492a12e04db11
WonyuChoi <phillip07290@naver.com>
2024-05-30 18:37:44 +0900

    refactor: programmers/random_2nd/Pr138476

--- src/Programmers/random_2nd/Pr138476.java
+++ src/Programmers/random_2nd/Pr138476.java
@@ -7,19 +7,14 @@ public class Pr138476 {
     public int solution(int k, int[] tangerine) {
         Map<Integer, Integer> m = new HashMap<>();
         for (int i : tangerine) {
-            if (m.containsKey(i)) {
-                m.put(i, m.get(i) + 1);
-            } else {
-                m.put(i, 1);
-            }
+            m.put(i, m.getOrDefault(i, 0) + 1);
         }
         List<Integer> values = new ArrayList<>(m.values());
-        Collections.sort(values);
+        values.sort(Collections.reverseOrder());
 
         int answer = 0;
-        for (int i = values.size() - 1; i >= 0; i--) {
-            int n = values.get(i);
-            k -= n;
+        for (int i : values) {
+            k -= i;
             answer++;
             if (k <= 0) {
                 break;
