cab235a873dc936cef6c577c905b73819fc8c29b
WonyuChoi <phillip07290@naver.com>
2025-03-29 15:16:28 +0900

    feat: BOJ/random/BOJ2493 stack

--- src/BOJ/random/BOJ2493.java
+++ src/BOJ/random/BOJ2493.java
@@ -3,7 +3,20 @@ package BOJ.random;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
+
+class Tower {
+
+    int num;
+    int height;
+
+    public Tower(int num, int height) {
+        this.num = num;
+        this.height = height;
+    }
+}
 
 public class BOJ2493 {
 
@@ -12,30 +25,20 @@ public class BOJ2493 {
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] towers = new int[n + 1];
-        towers[0] = Integer.MAX_VALUE;
-        for (int i = 1; i <= n; i++) {
-            towers[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int prevValue = 0;
+        Deque<Tower> towers = new LinkedList<>();
         int answer = 0;
-        int cur, prev;
         for (int i = 1; i <= n; i++) {
-            cur = towers[i];
-            prev = towers[i - 1];
-            if (cur > prev) {
-                for (int j = prevValue; j >= 0; j--) {
-                    if (towers[j] >= cur) {
-                        answer = j;
-                        break;
-                    }
-                }
-            } else {
-                answer = i - 1;
+            Tower cur = new Tower(i, Integer.parseInt(st.nextToken()));
+            while (!towers.isEmpty() && towers.peekLast().height < cur.height) {
+                towers.pollLast();
             }
+            if (towers.isEmpty()) {
+                answer = 0;
+            } else {
+                answer = towers.peekLast().num;
+            }
+            towers.add(cur);
             sb.append(answer).append(" ");
-            prevValue = answer;
         }
         System.out.println(sb);
     }
