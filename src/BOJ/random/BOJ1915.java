8c1869afbc842c457e20c047a10a94b69bf42bf6
WonyuChoi <phillip07290@naver.com>
2025-08-30 13:01:47 +0900

    feat: BOJ/random/BOJ1915

---
+++ src/BOJ/random/BOJ1915.java
@@ -0,0 +1,41 @@
+package BOJ.random;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1915 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        int[][] dp = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = input.charAt(j) - '0';
+            }
+        }
+
+        int answer = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 0) {
+                    continue;
+                }
+                if (i == 0 || j == 0) {
+                    dp[i][j] = arr[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                }
+                answer = Math.max(answer, dp[i][j]);
+            }
+        }
+        System.out.println(answer * answer);
+    }
+}
